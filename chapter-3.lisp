8e13496ee60965d0edf2318e0212ec609099ba3d
Toby Law <toby@tzcl.me>
2022-06-06 19:51:09 +1000

    Almost finished with Ch 3 exercises

--- chapter-3.lisp
+++ chapter-3.lisp
@@ -2,12 +2,19 @@
 
 ;;; Definitions
 ;; A list is the empty list or a cons cell
-(defun our-listp (x)
-  (or (null x) (consp x)))
+(defun our-listp (xs)
+  (or (null xs) (consp xs)))
 
 ;; An atom is anything that is not a cons
 ;; (the empty list is both an atom and a list)
-(defun our-atom (x) (not (consp x)))
+(defun our-atom (xs)
+  (not (consp xs)))
+
+;; A proper list is where the cdr is another list
+;; (as opposed to a dotted list, where the cdr is not another list)
+(defun proper-list? (xs)
+  (or (null xs)
+      (and (consp xs) (proper-list? (cdr xs)))))
 
 ;;; Equality
 ;; equal, essentially, returns true if its arguments print the same
@@ -91,18 +98,135 @@
   (if (our-member x xs) xs
       (cons x xs)))
 
+;;; Stacks
+;; push and pop are macros that make lists behave like stacks
+(defun our-reverse (xs)                 ; iterative reverse using push
+  (let ((acc '()))
+    (dolist (x xs) (push x acc))
+    acc))
+
+;; pushnew is like push but uses adjoin instead of cons
+
+;;; alists
+;; assoc finds the value associated with a given key, or returns nil
+;; the real assoc takes keyword arguments like member
+(defun our-assoc (key alist)
+  (and (consp alist)
+       (let ((pair (car alist)))
+         (if (eql key (car pair)) pair
+             (our-assoc key (cdr alist))))))
+
 ;;;; Exercises
 ;; define union such that it respects the ordering of elements
-(defun new-union (list1 list2)
-  (dolist (e list2 list1)
-    (print e)
-    (pushnew e list1)))
-
-(defun new-union (list1 list2)
+;; the idea is to append new items from the second list to the first list
+;; clean solution: use pushnew, iteration?
+(defun new-union (list1 list2)          ; recursive, but inefficient
   (cond ((null list2) list1)
         ((not (member (car list2) list1))
          (append (new-union list1 (cdr list2)) (list (car list2))))
         (t (new-union list1 (cdr list2)))))
 
-(equal (new-union '(a b c) '(b a d)) '(a b c d))
-(union '(a b c) '(b a d))
+;; better solution? more efficient
+(defun new-union1 (list1 list2)
+  (let ((rlist1 (reverse list1)))
+    (dolist (e list2 (nreverse rlist1))
+      (pushnew e rlist1))))
+
+;; return elements and their freq in sorted order
+;; the optimal solution would use a hashmap, but good chance to use an alist
+;; is there a nice way to do this recursively?
+(defun freq (xs)
+  (let ((alist '()))
+    (dolist (x xs (sort alist #'> :key #'cdr))
+      (let ((count (our-assoc x alist)))
+        (if count (incf (cdr count))
+            (push (cons x 1) alist))))))
+
+;; more idiomatic soln
+(defun freq1 (xs)
+  (let* ((uniq (remove-duplicates xs))
+         (alist (pairlis uniq (make-list (length uniq) :initial-element 0))))
+    (loop for x in xs
+          do (incf (cdr (assoc x alist))))
+    (sort alist #'> :key #'cdr)))
+
+;; pos+
+(defun pos+ (xs &optional (index 0))    ; recursive
+  (if (null xs) '()
+      (cons (+ (car xs) index)
+            (pos+ (cdr xs) (1+ index)))))
+
+(defun pos+ (xs)                        ; iterative
+  (let ((acc '()))
+    (dolist (x xs)
+      (push (+ x (length acc)) acc))    ; length is O(n)
+    (nreverse acc)))
+
+(defun pos+ (xs)                        ; iterative
+  (loop for x in xs
+        for i from 0
+        collect (+ x i)))
+
+(defun range (max &key (min 0) (step 1))
+  (loop for i from min below max by step
+        collect i))
+
+(defun pos+ (xs)                        ; mapcar
+  (mapcar #'+ xs (range (length xs))))
+
+;; Optimising run-length encoding
+(defun compress (x)
+  (if (consp x)
+      (compr (car x) 1 (cdr x))
+      x))
+
+(defun compr (x n xs)
+  (if (null xs)
+      (list (n-elts x n))
+      (let ((next (car xs)))
+        (if (eql next x)
+            (compr x (1+ n) (cdr xs))
+            (cons (n-elts x n)
+                  (compr next 1 (cdr xs)))))))
+
+(defun n-elts (x n)
+  ;; using a dotted list instead of a proper list saves a cons per item
+  (if (> n 1) (cons n x) x))
+
+(defun uncompress (xs)
+  (if (null xs) '()
+      (let ((x (car xs))
+            (rest (uncompress (cdr xs))))
+        (if (atom x)
+            (cons x rest)
+            (append (make-list (car x) :initial-element (cdr x))
+                    rest)))))
+
+;; Takes a list and prints it in dot notation
+(defun showdots (xs)
+  (format t "~a" (showdots-recur xs)))
+
+(defun showdots-recur (xs)
+  (if (null xs) 'nil
+      (format nil "(~a . ~a)" (car xs) (showdots-recur (cdr xs)))))
+
+;; longest path
+(setf network '((a b c) (b c) (c d)))
+
+(defun bfs (end queue net)
+  (if (null queue) nil
+      (let* ((path (car queue))
+             (node (car path)))
+        (print queue)
+        (if (eql end node) (reverse path)
+            (bfs end
+                 (append (cdr queue)
+                         (new-paths path node net))
+                 net)))))
+
+(defun new-paths (path node net)
+  (mapcar #'(lambda (n) (cons n path))
+          (cdr (assoc node net))))
+
+(defun shortest-path (start end net)
+  (bfs end (list (list start)) net))
